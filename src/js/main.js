7aa44207c6653bdc3d3a71f789a03ada38d41af4
GuidoP97 <guidopodesta99@gmail.com>
2021-04-03 19:55:49 -0300

    add main.js and remove attribute open

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+const contenedor = document.querySelector('.card-body');
+
+const [...articulos] = contenedor.children
+
+console.log(articulos);
+let newClick =  '',
+lastClick = '';
+
+articulos.map(({ children }) => {
+  const [...detailsTag] = children;
+  const contenedorEvento = detailsTag[0]
+
+  contenedorEvento.addEventListener('click', e => {
+    if(lastClick === ''){
+      newClick = e.target.parentElement;
+      lastClick = newClick;
+    } else if(newClick !== e.target.parentElement) {
+      lastClick = newClick;
+      newClick = e.target.parentElement;
+      lastClick.removeAttribute('open')
+    }
+    console.log(newClick, lastClick);
+  })
+})
